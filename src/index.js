38f5b40840352b13ddb628c9829b744d8235710a
Chris Olah <colah@openai.com>
2020-09-21 23:22:14 -0700

    Add initial work on universality section

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import * as _unused from "raw-loader!./index.ejs";
 // TODO: disable before publishing
 
 import Figure from "./diagrams/Neuron.svelte";
+import UniversalityTable from "./diagrams/UniversalityTable.svelte";
 import "regenerator-runtime/runtime";
 
 // lazily initialize any diagram below the fold. E.G:
-const exampleTag = document.getElementById("fig1");
-exampleTag.addEventListener("ready", () => {
+// const exampleTag = document.getElementById("fig1");
+// exampleTag.addEventListener("ready", () => {
 
-	const target = exampleTag.querySelector("#fig_inner");
-	let example = new Figure({ target, props: 
-								{
-									neuron: 1,
-									layername: "4/4/Add_6",
-									model: "RN_4x"
-								}
-							});
+// 	const target = exampleTag.querySelector("#fig_inner");
+// 	let example = new Figure({ target, props: 
+// 								{
+// 									neuron: 1,
+// 									layername: "4/4/Add_6",
+// 									model: "RN_4x"
+// 								}
+// 							});
 
-});
+
+
+// });
+
+
+const target = document.getElementById("universality-diagram");
+let example = new UniversalityTable({ target, props: 
+							{
+							}
+						});
+
