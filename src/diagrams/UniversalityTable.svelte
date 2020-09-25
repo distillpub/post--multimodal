b8c05d3b4e872e1747c773eddfb6ac1913ba45d7
Chris Olah <colah@openai.com>
2020-09-24 20:28:53 -0700

    move universality table to named css grid

--- src/diagrams/UniversalityTable.svelte
+++ src/diagrams/UniversalityTable.svelte
@@ -3,19 +3,20 @@
     @media only screen and (min-width: 1400px) {
         .table {
             grid-gap: 12px;
-            grid-template-columns: 160px 160px repeat(6, 164px);
+            grid-template-columns: [category] 160px [label] 160px repeat(6, [main] 164px);
         }
     }
     @media only screen and (max-width: 1399px){
         .table {
             display: grid;
             grid-column-gap: 12px;
-            grid-template-columns: 120px 100px repeat(6, 86px);
+            grid-template-columns: [category] 120px [label] 100px repeat(6, [main] 86px);
         }
     }
     .table {
         display: grid;
         grid-row-gap: 16px;
+        grid-template-rows: [category] auto [label] auto repeat(6, [main] auto) [caption] auto;
     }
     .feature-box {
         display: flex; flex-wrap: wrap; grid-gap: 2px; align-content: start;
@@ -103,7 +104,7 @@
             ]
         },
         {
-            "name": "'f' Detectors",
+            "name": "'F' Detectors",
             "description": "Units which detect birds (mot found in Places).",
             "model_features": [
                 [],
@@ -138,7 +139,7 @@
     </div>-->
 
     {#each columnTitles as column, i}
-    <div style='grid-column:{3+i}; grid-row:2; padding-bottom: 8px; /*border-bottom: 1px solid #CCC;*/'>
+    <div style='grid-column: main {i+1}; grid-row: label; padding-bottom: 8px; /*border-bottom: 1px solid #CCC;*/'>
         {column.header}
         <div class='figcaption'>{column.sub}</div>
     </div>
@@ -146,17 +147,17 @@
 
 
     {#each rows as row, j}
-    <div style='grid-column:2; grid-row:{3+j}; padding-right: 10px;'>
+    <div style='grid-column: label; grid-row: main {j+1}; padding-right: 10px;'>
         <div style='line-height: 120%; margin-bottom: 8px;'>{row.name}</div>
         <div class='figcaption'>{row.description}</div>
     </div>
     {#each row.model_features as features, i}
     {#if features.length == 0}
-    <div style='grid-column:{3+i}; grid-row:{3+j}; background: #EEE; border-radius: 2px; ' class="feature-box">
+    <div style='grid-column: main {i+1}; grid-row: main {j+1}; background: #EEE; border-radius: 2px; ' class="feature-box">
         <div class="figcaption" style='margin:auto; margin-top: 24px; max-width: 120px; opacity: 0.7;'>No correlated features.</div>
     </div>
     {:else}
-    <div style='grid-column:{3+i}; grid-row:{3+j};' class="feature-box">
+    <div style='grid-column: main {i+1}; grid-row: main {j+1};' class="feature-box">
         {#each features as feature}
         <a href="{feature.a_url}" >
         <img src='{feature.img_url}' alt='{feature.model} {feature.layer}:{feature.unit}'>
@@ -167,23 +168,25 @@
     {/each}
     {/each}
 
-    <div style='grid-column:1; grid-row-start:3; grid-row-end:5; padding-right: 16px; margin-right: 16px; border-right: 1px solid #CCC;'>
+    <div style='grid-column: category; grid-row-start: main 1; grid-row-end: main 3; padding-right: 16px; margin-right: 16px; border-right: 1px solid #CCC;'>
         <div style='line-height: 120%; margin-bottom: 8px;'>Universal Features</div>
         <div class='figcaption'>Features which are shared across ImageNet, Places365, im2gps and Multimodal models.</div>
     </div>
-    <div style='grid-column:1; grid-row-start:5; grid-row-end:7; padding-right: 16px; margin-right: 16px; border-right: 1px solid #CCC;'>
+    <div style='grid-column: category; grid-row-start: main 3; grid-row-end: main 5; padding-right: 16px; margin-right: 16px; border-right: 1px solid #CCC;'>
         <div style='line-height: 120%; margin-bottom: 8px;'>Multimodal + One Other Model</div>
         <div class='figcaption'>Features which are shared across ImageNet, Places365, im2gps and Multimodal models.</div>
     </div>
-    <div style='grid-column:1; grid-row-start:7; grid-row-end:8; padding-right: 16px; margin-right: 16px; border-right: 1px solid #CCC;'>
+    <div style='grid-column: category; grid-row-start: main 5; grid-row-end: main 6; padding-right: 16px; margin-right: 16px; border-right: 1px solid #CCC;'>
         <div style='line-height: 120%; margin-bottom: 8px;'>Multimodal Only</div>
         <div class='figcaption'>Features which found in Multimodal models, but not ImageNet, Places365, or im2gps.</div>
     </div>
 
+    <div style='margin-top:16px; max-width: 784px; grid-row: caption; grid-column: main 1 / main 5;' class="figcaption">
+        <a class="figure-anchor" href="#universality-diagram">Figure N:</a>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+
 </div>
-<div style='margin-top:32px; max-width: 784px;' class="figcaption">
-    <a class="figure-anchor" href="#universality-diagram">Figure N:</a>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
+
 </div>
 
