c56f1e86b5350d4f639fac99b59ab4be403210a0
Chelsea Voss <csvoss@openai.com>
2020-10-02 18:13:15 +0100

    Add ClassificationCard component

--- src/diagrams/InTheWild2.svelte
+++ src/diagrams/InTheWild2.svelte
@@ -68,6 +68,15 @@ let adversarialLabels = {
   "defaultHue": 0,
 }
 
+function hues(label) {
+  return adversarialLabels[label].labels.reduce(function(output, trueLabel) {
+    output[trueLabel] = adversarialLabels[label].hue;
+    return output;
+  }, {});
+}
+
+
+import ClassificationCard from '../components/ClassificationCard.svelte';
 </script>
 
 <div style="max-width: 1440px; margin: auto;">
@@ -77,19 +86,13 @@ let adversarialLabels = {
     {/each}
     {#each Object.entries(attacks_2) as [item, labels], row_index}
       {#each Object.entries(labels) as [label, results], col_index}
-        <div style="display: flex; flex-direction: row; border-radius: 6px; overflow: hidden; width: fit-content; height: 174px; border: 1px solid #EEE; grid-column: {col_index + 1 }">
-          <div style="border-right: 1px solid #EEE">
-            <img style="width: 174px;" src="{results.image_url }?cache=26" alt="{item} labeled {label}"/>
-          </div>
-          <div>
-            {#each results.zero_shot_statistics as probability}
-              <div style="border-bottom: 1px solid #EEE; background-color: hsla({ adversarialLabels[label].labels.includes(probability[1]) ? adversarialLabels[label].hue : adversarialLabels.defaultHue }, { (trueLabels[item].includes(probability[1]) && !adversarialLabels[label].labels.includes(probability[1])) || !adversarialLabels[label].labels.includes(probability[1]) ? "0%" : "70%" } , 50%, { probability[0]}); color: #{ probability[0] < 0.7 ? "000000" : "FFFFFF"}; padding: 0px 10px; line-height: 16px; width: 145px; font-size: 80%">
-                <small>{probability[1] }
-                  <span style="float: right;">{Math.round(probability[0] * 10000) / 100 }%</span>
-                </small>
-              </div>
-            {/each}
-          </div>
+        <div style="grid-column: {col_index + 1 }">
+          <ClassificationCard
+            imageUrl={results.image_url}
+            imageAltText={`${item} labeled ${label}`}
+            probabilities={results.zero_shot_statistics.slice(0, 9)}
+            customHues={hues(label)}
+          />
         </div>
       {/each}
       <br/>
