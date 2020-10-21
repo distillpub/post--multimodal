89ed2725906aef67c1bf84e087808cb0086f4e44
Chelsea Voss <csvoss@openai.com>
2020-10-22 00:04:42 +0200

    Update AttackableNeurons.svelte to adjust for being in the margin
    Also, add another neuron to the mix.

--- src/diagrams/AttackableNeurons.svelte
+++ src/diagrams/AttackableNeurons.svelte
@@ -1,15 +1,23 @@
+<script>
+import MicroscopeButton from '../components/MicroscopeButton.svelte'
+</script>
 <style>
   .reading-container {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     width: 100%;
   }
   .reading-neuron-feature-viz {
-    border-radius: 1em;
+    border-radius: 5px;
   }
   .reading-neuron-dataset-example {
-    max-width: 48%;
     margin: 0px;
+  }
+  .reading-neuron-dataset-examples {
+    display: grid;
+    grid-template-rows: repeat(2);
+    grid-template-columns: repeat(2);
+    grid-gap: 2px;
   }
   .reading-neuron {
     width: 30%;
@@ -18,22 +26,37 @@
 </style>
 <div class="reading-container">
   <div class="reading-neuron">
-    <h4><a href="https://ggoh-staging-dot-encyclopedia-251300.wl.r.appspot.com/models/contrastive_4x/image_block_4_5_Add_6_0/1330">RN50-4x Channel 1330</a></h4>
-    <img class="reading-neuron-feature-viz" src="/typographic/RN50-4x-channel-1330.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1330-dataset-1.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1330-dataset-2.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1330-dataset-3.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1330-dataset-4.png" alt=""/>
-    <p><small>"price", "cheap", "$"</small></p>
+    <p><small>price, cheap, $</small></p>
+    <img class="reading-neuron-feature-viz" src="typographic/RN50-4x-channel-1330.png" alt=""/>
+    <div class="reading-neuron-dataset-examples">
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1330-dataset-1.png" alt="" style="grid-row: 1; grid-column: 1;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1330-dataset-2.png" alt="" style="grid-row: 1; grid-column: 2;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1330-dataset-3.png" alt="" style="grid-row: 2; grid-column: 1;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1330-dataset-4.png" alt="" style="grid-row: 2; grid-column: 2;"/>
+    </div>
+    <MicroscopeButton style="float: right" data-unit="1330" data-model="4x" data-layer="image_block_4_5_Add_6_0"/>
   </div>
   <div class="reading-neuron">
-    <h4><a href="https://ggoh-staging-dot-encyclopedia-251300.wl.r.appspot.com/models/contrastive_4x/image_block_4_5_Add_6_0/1450">RN50-4x Channel 1450</a></h4>
-    <img class="reading-neuron-feature-viz" src="/typographic/RN50-4x-channel-1450.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1450-dataset-1.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1450-dataset-2.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1450-dataset-3.png" alt=""/>
-    <img class="reading-neuron-dataset-example" src="/typographic/RN50-4x-1450-dataset-4.png" alt=""/>
-    <p><small>"iPod", "iOS", "iPhone"</small></p>
+    <p><small>iPod, iOS, iPhone</small></p>
+    <img class="reading-neuron-feature-viz" src="typographic/RN50-4x-channel-1450.png" alt=""/>
+    <div class="reading-neuron-dataset-examples">
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1450-dataset-1.png" alt="" style="grid-row: 1; grid-column: 1;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1450-dataset-2.png" alt="" style="grid-row: 1; grid-column: 2;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1450-dataset-3.png" alt="" style="grid-row: 2; grid-column: 1;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1450-dataset-4.png" alt="" style="grid-row: 2; grid-column: 2;"/>
+    </div>
+    <MicroscopeButton data-unit="1450" data-model="4x" data-layer="image_block_4_5_Add_6_0"/>
+  </div>
+  <div class="reading-neuron">
+    <p><small>trash, waste, garbage</small></p>
+    <img class="reading-neuron-feature-viz" src="typographic/RN50-4x-channel-1156.png" alt=""/>
+    <div class="reading-neuron-dataset-examples">
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1156-dataset-1.png" alt="" style="grid-row: 1; grid-column: 1;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1156-dataset-2.png" alt="" style="grid-row: 1; grid-column: 2;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1156-dataset-3.png" alt="" style="grid-row: 2; grid-column: 1;"/>
+      <img class="reading-neuron-dataset-example" src="typographic/RN50-4x-1156-dataset-4.png" alt="" style="grid-row: 2; grid-column: 2;"/>
+    </div>
+    <MicroscopeButton data-unit="1156" data-model="4x" data-layer="image_block_4_5_Add_6_0"/>
   </div>
 </div>
 <div class="figcaption">
