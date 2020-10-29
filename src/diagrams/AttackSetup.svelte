06f5286a00f795badd757ea5f619461d9c5d5d37
Chelsea Voss <csvoss@openai.com>
2020-10-29 23:50:11 +0100

    Add a tiny tweak to AttackSetup.svelte's caption

--- src/diagrams/AttackSetup.svelte
+++ src/diagrams/AttackSetup.svelte
@@ -49,7 +49,7 @@ import ClassificationCard from '../components/ClassificationCard.svelte';
   </div>
   <div class="figcaption" style="grid-column: 1/4; grid-row: 2; max-width: 700px">
     <p>
-      <a class="figure-anchor" href="#attack-setup">Figure N:</a> An example of the simple automated attack setup for the target class <code>pizza</code> with the attack text <i>pizza</i>. Attacks were generated using the same (arbitrarily chosen) eight coordinates and using a consistent font style, as shown here.
+      <a class="figure-anchor" href="#attack-setup">Figure N:</a> An example of the fixed automated attack setup for the target class <code>pizza</code> with the attack text <i>pizza</i>. Attacks were generated using the same (arbitrarily chosen) eight coordinates and using a consistent font style, as shown here.
 
       We consider an attack to have succeeded if the top class was changed to the attack class.
     </p>
