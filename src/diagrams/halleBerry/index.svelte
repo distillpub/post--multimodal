6a14951546079f43221d85b5d6e16b14acae8768
Chris Olah <colah@openai.com>
2020-11-02 19:45:19 -0800

    Update intro diagram swap Trump for Spiderman

--- src/diagrams/halleBerry/index.svelte
+++ src/diagrams/halleBerry/index.svelte
@@ -1,20 +1,26 @@
-<figure
-  style="
+
+<style>
+  .container {
     display: grid;
-    grid-column-gap: 64px;
+    grid-column-gap: 40px;
     grid-row-gap: 16px;
     grid-template-columns: repeat(2, auto);
-    max-width: 600px;
-  "
->
+    max-width: 580px;
+  }
+  .container > img {
+    max-width: 280px;
+  }
+</style>
+
+<figure class='container'>
   <div class="figcaption">
     Just as the biological Halle Berry neuron responds to photos, drawings,
     and text...
   </div>
   <div class="figcaption">
-    ... multimodal neurons in artificial neural networks activate for
-    photos, cartoons, and text as well.
+    ... an artificial spiderman neruon activates for
+    photos, drawings, and text as well.
   </div>
-  <img src="images/IntroHalleBerry.png" />
-  <img src="images/IntroTrump.png" />
+  <img src="images/Intro3HalleBerry.png" />
+  <img src="images/Intro3Spiderman.png" />
 </figure>
