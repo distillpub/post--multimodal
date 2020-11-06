0dde9cb93ac41f1d16a398c94ff0725a8cd04d78
Chris Olah <colah@openai.com>
2020-11-06 09:22:05 -0800

    add geo_ref() function

---
+++ src/util_mdx.js
@@ -0,0 +1,6 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import {microscope_url, map_url, facet_icon_url, dataset_examples_url} from './urls.js';
+
+export function geo_ref(text, view=0, focus=null, map="geography") {
+    return <a href="#region-neuron-diagram" onClick={()=>{window.setRegionalState(map, view, focus)}}>{text}</a>
+}
