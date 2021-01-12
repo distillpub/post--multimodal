facdec5d6da522ce82a49c111d106f7deb60e294
Chris Olah <christopherolah.co@gmail.com>
2021-01-11 16:58:20 -0800

    update microscope alert text

--- src/diagrams/MicroscopeAlert.svelte
+++ src/diagrams/MicroscopeAlert.svelte
@@ -26,7 +26,7 @@
 <a href="https://ggoh-staging-dot-encyclopedia-251300.wl.r.appspot.com/models">
 <div class='microscope-alert'>
     <svg viewBox="0 20 65.51 73.37" width="100px" height="60px"><path fill="white" d="M32.78,24.48a6.11,6.11,0,1,0-6.11-6.11A6.12,6.12,0,0,0,32.78,24.48ZM62.92,61.74H57.33l-2.38-6H21.15a.22.22,0,0,1-.36-.06.43.43,0,0,1-.08-.39l5.73-22.08a4.75,4.75,0,0,1,.44-.68l3.56,0,2.43-5.19,0,5.21v7.64a3,3,0,0,0,3,3h.17l1.75,3.72h5.49L45,43.17h.17a3,3,0,0,0,3-3V6.06a3,3,0,0,0-3-3L45.46,3,44.38,0H36.53L35.45,3l.25.07a3,3,0,0,0-3,3V9.83h.07a8.54,8.54,0,1,1-7.78,12H19.89L7.28,61.74H2.59L0,73.37H8s1.08-5.26,1.28-3.43H56.23c.2-1.83,1.28,3.43,1.28,3.43h8Z"></path></svg>
-    <div>In this paper, we use feature visualizations as a kind of variable name to represent neurons. Whenever you see a feature visualization, <b>you can click on it to open that neuron in Microscope</b>. This will give you detailed information on it, including hundreds of dataset examples. You can also browse Microscope to see every neuron in the model.<br><br>
+    <div>In this paper, we use feature visualizations as a kind of variable name to represent neurons. Whenever you see a feature visualization, <b>you can click on it to open that neuron in Microscope</b>. This will give you detailed information on it, including dataset examples. (Note that dataset examples are only over ImageNet, however -- if a neuron responds to something not represented in ImageNet, dataset examples may be misleading.) You can also browse Microscope to see every neuron in the model.<br><br>
     Login info for reviewers -- user: "clarity" pw: "microscope" </div>
 </div>
 </a>
